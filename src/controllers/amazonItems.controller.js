0e473cd32997879ad3099554108d684291fdfcb1
hari <haridarshanofficial@gmail.com>
2022-06-18 21:29:49 +0530

    chore: added eslint and husky

--- src/controllers/amazonItems.controller.js
+++ src/controllers/amazonItems.controller.js
@@ -1,17 +1,17 @@
 const { scrapeData } = require('../services/amazonItems.service');
 
 async function getItems(req, res, next) {
-	try {
-		const results = await scrapeData(
-			`https://www.amazon.in/s?k=${req.params.query}`
-		);
-		res.status(200).json(results);
-	} catch (err) {
-		console.error(`Error while getting programming languages`, err.message);
-		next(err);
-	}
+  try {
+    const results = await scrapeData(
+      `https://www.amazon.in/s?k=${req.params.query}`,
+    );
+    res.status(200).json(results);
+  } catch (err) {
+    console.error('Error while getting programming languages', err.message);
+    next(err);
+  }
 }
 
 module.exports = {
-	getItems,
+  getItems,
 };
